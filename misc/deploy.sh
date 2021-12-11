ae07e33b1123805733763d370276cb5e67b0896d
Edinson E. Padrón Urdaneta <epadron+git@posteo.com>
2021-12-10 21:19:09 -0500

    Add XCompose file
    Signed-off-by: Edinson E. Padrón Urdaneta <edinson.padron+git@posteo.net>

---
+++ misc/deploy.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+export configuration_files_absolute_path=$(dirname $(readlink -f $0))
+export target_directory=$HOME
+
+mkdir -vp $target_directory
+
+ln -vis $configuration_files_absolute_path/XCompose $target_directory/.XCompose
