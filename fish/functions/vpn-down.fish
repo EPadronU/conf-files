ac50fca8224b04a0b7a9452a807d4180a0caf429
Edinson E. Padrón Urdaneta <edinson.padron+git@posteo.net>
2022-03-20 10:14:27 -0500

    Delete no-longer-use fish functions
    Signed-off-by: Edinson E. Padrón Urdaneta <edinson.padron+git@posteo.net>

--- fish/functions/vpn-down.fish
+++
@@ -1,8 +0,0 @@
-###############################################################################
-# https://tuxdiary.com/2014/09/04/cisco-anyconnect-vpn-with-openvpn-openconnect/
-###############################################################################
-
-function vpn-down
-  sudo ifconfig tun1 down; and \
-  sudo openvpn --rmtun --dev tun1
-end
