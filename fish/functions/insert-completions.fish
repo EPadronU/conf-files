35e8869075c33c02e1521749462544c0ba429e94
Edinson E. Padrón Urdaneta <epadron+git@posteo.com>
2022-03-20 10:14:27 -0500

    Delete no-longer-use fish functions
    Signed-off-by: Edinson E. Padrón Urdaneta <edinson.padron+git@posteo.net>

--- fish/functions/insert-completions.fish
+++
@@ -1,4 +0,0 @@
-function insert-completions
-  # We need to remove the descriptions, and we also don't want everything to be inserted on separate lines
-  commandline -i -- (complete --do-complete | string replace -r '\t.*' '' | string join " ")
-end
