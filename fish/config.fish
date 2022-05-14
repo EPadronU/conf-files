2833a9eaa87e030ce6356a7501b3590e54cd0d62
Edinson E. Padrón Urdaneta <edinson.padron+git@posteo.net>
2022-05-14 11:36:46 -0500

    Cleanup my $HOME directory
    Signed-off-by: Edinson E. Padrón Urdaneta <edinson.padron+git@posteo.net>

--- fish/config.fish
+++ fish/config.fish
@@ -5,6 +5,7 @@
 alias grep 'grep -E'
 alias sql sqlite3
 alias cat 'bat --pager=never'
+alias wget 'wget --hsts-file=$XDG_DATA_HOME/wget-hsts'
 
 ### Java-related
 alias mvn-generate-new-project 'mvn \
@@ -31,19 +32,43 @@ if status --is-interactive
     abbr --add --global gam 'git commit --amend --no-edit --date=now'
     abbr --add --global glg 'git log --graph --all'
     abbr --add --global gln 'git log --name-only'
+    abbr --add --global gpf 'git push --force'
+    abbr --add --global nvi 'nvm install lts'
 end
 
 ## Login shell configuration ##################################################
 if status --is-login
-    fish_add_path ~/.local/bin
-    fish_add_path ~/go/bin
+    # XDG standard
+    set -gx XDG_CACHE_HOME $HOME/.cache
+    set -gx XDG_CONFIG_HOME $HOME/.config
+    set -gx XDG_DATA_HOME $HOME/.local/share
+    set -gx XDG_STATE_HOME $HOME/.local/state
+    set -gx XDG_DATA_DIRS $XDG_DATA_DIRS:/var/lib/flatpak/exports/share:/home/nodxine/.local/share/flatpak/exports/share
 
+    # Preferred utilities
     set -gx BROWSER (type -p firefox)
-    set -gx EDITOR (type -p nvim)
+    set -gx VISUAL (type -p nvim)
+    set -gx EDITOR $VISUAL
+    set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
+
+    # Cleanup my $HOME directory
+    set -gx CARGO_HOME $XDG_DATA_HOME/cargo
+    set -gx GNUPGHOME $XDG_DATA_HOME/gnupg
+    set -gx GOPATH $XDG_DATA_HOME/go
+    set -gx GTK2_RC_FILES $XDG_CONFIG_HOME/gtk-2.0/gtkrc
+    set -gx KDEHOME $XDG_CONFIG_HOME/kde
+    set -gx LESSHISTFILE $XDG_DATA_HOME/lesshst
+    set -gx NPM_CONFIG_USERCONFIG $XDG_CONFIG_HOME/npm/npmrc
+    set -gx XCOMPOSEFILE $XDG_CONFIG_HOME/X11/XCompose
+    set -gx _JAVA_OPTIONS -Djava.util.prefs.userRoot=$XDG_CONFIG_HOME/java
+
+    # PATH
+    fish_add_path ~/.local/bin
+    fish_add_path $GOPATH/bin
+
+    # Program's tweaks
     set -gx MOZ_X11_EGL 1
     set -gx fish_browser $BROWSER
     set -gx fish_help_browser $BROWSER
     set -gx LESS --quit-if-one-screen --hilite-search --ignore-case --status-column --LONG-PROMPT --RAW-CONTROL-CHARS --chop-long-lines --no-histdups --save-marks
-    set -gx MANPAGER "sh -c 'col -bx | bat -l man -p'"
-    set -gx XDG_DATA_DIRS $XDG_DATA_DIRS:/var/lib/flatpak/exports/share:/home/nodxine/.local/share/flatpak/exports/share
 end
