bd6f2d4260b9527dc1bd9a271d4b4f98a553bd7e
Edinson E. Padrón Urdaneta <edinson.padron+git@posteo.net>
2023-07-05 11:28:55 -0500

    Use ufo for folding instead of treesitter

--- nvim/lua/nodxine/plugins/lsp/handlers.lua
+++ nvim/lua/nodxine/plugins/lsp/handlers.lua
@@ -94,15 +94,24 @@ M.on_attach = function(client, bufnr)
 end
 
 -- cmp integration --------------------------------------------------------------------------------
-local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
+local cmp_nvim_lsp_status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 
-if status_ok then
+if cmp_nvim_lsp_status_ok then
   -- Neovim's default omnifunc has basic support for serving completion candidates. nvim-cmp
   -- supports more types of completion candidates, so users must override the capabilities sent to
   -- the server such that it can provide these candidates during a completion request.
   M.capabilities = cmp_nvim_lsp.default_capabilities()
 else
   M.capabilities = vim.lsp.protocol.make_client_capabilities()
+end
+
+local ufo_status_ok, _ = pcall(require, "ufo")
+
+if ufo_status_ok then
+  M.capabilities.textDocument.foldingRange = {
+    dynamicRegistration = false,
+    lineFoldingOnly = true
+  }
 end
 
 return M
